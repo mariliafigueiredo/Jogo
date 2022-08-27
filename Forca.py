817ea8e86de0d72986729745158fe0d783e77dca
Marilia Figueiredo <mariliafigueiredo007@hotmail.com>
2022-08-27 11:45:28 -0300

    enviando repositorio para o github

---
+++ Forca.py
@@ -0,0 +1,58 @@
+
+def jogar():
+    print("*********************************")
+    print("****Bem vindo no jogo de Forca!**")
+    print("*********************************")
+
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+
+
+    arquivo.close()
+
+    print(palavras)
+
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+     #enquanto (True)
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros = erros + 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
